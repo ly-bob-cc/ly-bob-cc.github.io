7f4cd64a80f995806a2efd2d3a34796a3c269eb2
ly-bob-cc <117478308+ly-bob-cc@users.noreply.github.com>
2023-03-31 12:19:57 -0500

    Create idx.js
    Insert text into ids: _title, _h1

---
+++ docs/idx.js
@@ -0,0 +1,5 @@
+let tab_text = document.querySelector('#_title');
+tab_text.textContent = 'Coding Club';
+
+let h1_primary = document.querySelector('#_h1');
+h1_primary.textContent = 'Table of Content';
