5b527b6aa9afb1b40cf0a73246dc84e3aa76a38b
ly-bob-cc <117478308+ly-bob-cc@users.noreply.github.com>
2023-04-07 13:47:07 -0500

    Update idx.js

--- docs/idx.js
+++ docs/idx.js
@@ -6,7 +6,7 @@ h1_primary.textContent = 'Table of Content';
 
 let content = ['Kinematics Calculator', 'Pascal Triangle'];
 let ul_content = document.getElementById('_ul');
-for (let i = 0; 1 < content.length; i++) {
+for (let i = 0; i < content.length; i++) {
   let li_content = document.createElement('li');
   li_content.appendChild(document.createTextNode(content));
   ul_content.appendChild(li_content);
