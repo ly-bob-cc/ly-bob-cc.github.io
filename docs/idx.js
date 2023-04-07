76d32b073e44bd50b721ead9bf806a9112a29c71
ly-bob-cc <117478308+ly-bob-cc@users.noreply.github.com>
2023-04-07 13:43:02 -0500

    Update idx.js
    test dynamic text

--- docs/idx.js
+++ docs/idx.js
@@ -1,5 +1,12 @@
-let tab_text = document.querySelector('#_title');
+let tab_text = document.getElementById('_title');
 tab_text.textContent = 'Coding Club';
 
-let h1_primary = document.querySelector('#_h1');
+let h1_primary = document.getElementById('_h1');
 h1_primary.textContent = 'Table of Content';
+
+let content = ['Kinematics Calculator', 'Pascal Triangle'];
+let ul_content = document.getElementById('_ul');
+for (let i = 0; 1 < content.length; i++) {
+  let li_content = document.createElement('li');
+  li_content.appendChild(document.createTextNode(content));
+  ul_content.appendChild(li_content);
